62c50c26579976f8825370436aaf3fc67115dbc9
Neal <nohesselton@gmail.com>
2026-03-04 09:59:46 +0100

    Readability: Comment additions

--- movement/computer_players/computer_movement.py
+++ movement/computer_players/computer_movement.py
@@ -4,7 +4,7 @@ Computer movement middle man for main and registry
 
 from movement.computer_players.registry import computer_model_registry
 
-
+# Should return a move object
 def get_computer_move(current_player_index, board_lst, config):
     player_index = current_player_index
     algorithm_type = config.model_type[player_index]
--- movement/human_players/human_movement.py
+++ movement/human_players/human_movement.py
@@ -4,7 +4,7 @@ human movement middle man for main and registry
 
 from movement.human_players.registry import human_move_registry
 
-
+## Should return a player position
 def get_player_move(key, current_player_index, player_position: list, size: int, config):
     human_input_type = config.render_type
     move_function = human_move_registry[human_input_type]
