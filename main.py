df92dd97af337c0610c1482d9c65f60bf5a58e59
Neal <nohesselton@gmail.com>
2026-03-04 16:02:09 +0100

    Refact: Simplified main.py and separated/unloaded the overloaded main()

--- main.py
+++ main.py
@@ -16,34 +16,33 @@ from runtime.cli_runtime import CLIRuntime
 
 config = GameConfig()
 
+def run_cli(config):
+    import curses
+    runtime = CLIRuntime(config)
+    curses.wrapper(runtime.run)
 
-def main(stdscr, config):
-    if stdscr is not None:
-        runtime = CLIRuntime(config)
-        runtime.run(stdscr)
-    else:
-        engine = SimulationEngine(config)
-        result = engine.run()
+def run_headless(config):
+    engine = SimulationEngine(config)
+    result = engine.run()
 
-        print(result)
-        for game in result.runs:
-            print("GameRunContext Object:", game)
-            print("Game ID:", game.game_id)
-            print("Winner:", game.winner)
-            print("Moves:", len(game.moves))
-            print("Final Board:", game.moves[len(game.moves)-1]["board_state"])
-            print("--------")
+    print(result)
+    for game in result.runs:
+        print("GameRunContext Object:", game)
+        print("Game ID:", game.game_id)
+        print("Winner:", game.winner)
+        print("Moves:", len(game.moves))
+        print("Final Board:", game.moves[len(game.moves)-1]["board_state"])
+        print("--------")
 
+    return result
 
 
 # Easy setup and tear down
-if config.render:
-    if config.render_type == "cli":
-        import curses
+if config.render and config.render_type == "cli":
         try:
-            curses.wrapper(lambda stdscr: main(stdscr, config))
+            run_cli(config)
         except KeyboardInterrupt:
             print("Exiting through KeyboardInterrupt by user")
             exit()
 else:
-    main(None, config)
+    run_headless(config)
