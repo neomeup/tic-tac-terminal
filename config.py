6ec33f2e105bb51c3eeddd87967099dd8ce28beb
Neal <nohesselton@gmail.com>
2026-03-04 11:41:16 +0100

    Fix: Fixed overwriting of dataclass properties in config

--- config.py
+++ config.py
@@ -2,37 +2,37 @@ from dataclasses import dataclass
 
 @dataclass
 class GameConfig:
-    def __init__(self):
-        # Character for renders
-        self.x_char: str = "X"
-        self.o_char: str = "O"
-        self.empty_char: str = "-"
+    # Character for renders
+    x_char: str = "X"
+    o_char: str = "O"
+    empty_char: str = "-"
 
-        # Board size
-        self.board_size: int = 3
+    # Board size
+    board_size: int = 3
 
-        # Win length Tic-tac-toe/standard rule sets
-        self.win_length: int = 3 # Used for simple wins i.e. tic-tac-toe style
+    # Win length Tic-tac-toe/standard rule sets
+    win_length: int = 3 # Used for simple wins i.e. tic-tac-toe style
 
-        # Game type indicator
-        self.rule_set: str = "standard"
+    # Game type indicator
+    rule_set: str = "standard"
 
-        # Optional render flag
-        self.render: bool = False # Set False for non-rendered games
-        self.render_type: str = "cli"
+    # Optional render flag
+    render: bool = False # Set False for non-rendered games
+    render_type: str = "cli"
 
+    # Piece type
+    piece_type: str = "standard_x-o"
+
+    # Start conditions for movement
+    random_start: bool = True
+
+    # Games to be played in simulation mode
+    how_many_games: int = 3
+
+    # Random Seed Config
+    random_seed: int | None = 15
+
+    def __post_init__(self):
         # Player types
         self.player_types: list = ["computer", "computer", "computer"] # ["human", "computer"] -> will yeild single player mode
         self.model_type: list = ["random", "random", "random"]
-
-        # Piece type
-        self.piece_type: str = "standard_x-o"
-
-        # Start conditions for movement
-        self.random_start: bool = True
-
-        # Games to be played in simulation mode
-        self.how_many_games: int = 3
-
-        # Random Seed Config
-        self.random_seed: int | None = 15
