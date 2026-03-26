6be1963ad7cb08e8c659710958efe0fa81bf18cd
Neal <nohesselton@gmail.com>
2026-03-26 17:59:48 +0100

    Feat: Updated action encoder to also decode and optionally accept row/col instead of move object

--- simulation/training/encoding/encoders/action_encoder.py
+++ simulation/training/encoding/encoders/action_encoder.py
@@ -2,9 +2,17 @@
 Simple encoder to turn move objects into integers actions
 '''
 
-def encode_action(move, board_size):
+def encode_action(move, board_size, row=None, col=None):
+    if row and col:
+        action_encoded = (row * board_size) + col
+        return action_encoded
     if move is None:
         return None
     
     action_encoded = (move["row"] * board_size) + move["col"]
     return action_encoded
+
+def decode_action(encoded_value, board_size):
+    row = encoded_value // board_size
+    col = encoded_value % board_size
+    return row, col
