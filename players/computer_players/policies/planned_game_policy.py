99b8e5731f117ad494819a2e3ec4b51c8c8207ee
Neal <nohesselton@gmail.com>
2026-03-30 12:22:52 +0200

    Feat: planned policies implemented

--- players/computer_players/policies/planned_game_policy.py
+++ players/computer_players/policies/planned_game_policy.py
@@ -26,21 +26,47 @@ class PlannedGame(BasePolicy):
 
 
         first_move = self.is_x_move(board_lst, x_move = 1)
-        second_move = self.is_x_move(board_lst, x_move = 2)
+        #second_move = self.is_x_move(board_lst, x_move = 2)
         third_move = self.is_x_move(board_lst, x_move = 3)
-        fourth_move = self.is_x_move(board_lst, x_move = 4)
+        #fourth_move = self.is_x_move(board_lst, x_move = 4)
         fifth_move = self.is_x_move(board_lst, x_move = 5)
-        sixth_move = self.is_x_move(board_lst, x_move = 6)
+        #sixth_move = self.is_x_move(board_lst, x_move = 6)
         seventh_move = self.is_x_move(board_lst, x_move = 7)
-        eigth_move = self.is_x_move(board_lst, x_move = 8)
+        #eigth_move = self.is_x_move(board_lst, x_move = 8)
         ninth_move = self.is_x_move(board_lst, x_move = 9)
 
 
         if first_move:
+            r = 1
+            c = 1
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+            
+        if third_move:
+            r = 1
+            c = 2
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+
+        if fifth_move:
+            r = 2
+            c = 2
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+
+        if seventh_move:
             r = 2
             c = 0
             if board_lst[r][c] is None:
                 return Move (player_id=player_id, target_row=r, target_col=c)
+
+        if ninth_move:
+            r = 2
+            c = 1
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)   
+
+         
 
         for row_index, row in reversed(list(enumerate(board_lst))):
             for col_index, cell in reversed(list(enumerate(row))):
--- players/computer_players/policies/planned_game_policy2.py
+++ players/computer_players/policies/planned_game_policy2.py
@@ -25,23 +25,42 @@ class PlannedGameTwo(BasePolicy):
         player_id = current_player_index
 
 
-        first_move = self.is_x_move(board_lst, x_move = 1)
+        #first_move = self.is_x_move(board_lst, x_move = 1)
         second_move = self.is_x_move(board_lst, x_move = 2)
-        third_move = self.is_x_move(board_lst, x_move = 3)
+        #third_move = self.is_x_move(board_lst, x_move = 3)
         fourth_move = self.is_x_move(board_lst, x_move = 4)
-        fifth_move = self.is_x_move(board_lst, x_move = 5)
+        #fifth_move = self.is_x_move(board_lst, x_move = 5)
         sixth_move = self.is_x_move(board_lst, x_move = 6)
-        seventh_move = self.is_x_move(board_lst, x_move = 7)
+        #seventh_move = self.is_x_move(board_lst, x_move = 7)
         eigth_move = self.is_x_move(board_lst, x_move = 8)
-        ninth_move = self.is_x_move(board_lst, x_move = 9)
+        #ninth_move = self.is_x_move(board_lst, x_move = 9)
 
         
         if second_move:
-            r = 2
+            r = 0
+            c = 2
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+
+        if fourth_move:
+            r = 1
             c = 0
             if board_lst[r][c] is None:
                 return Move (player_id=player_id, target_row=r, target_col=c)
 
+        if sixth_move:
+            r = 0
+            c = 0
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+
+        if eigth_move:
+            r = 0
+            c = 1
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+        
+        
         for row_index, row in reversed(list(enumerate(board_lst))):
             for col_index, cell in reversed(list(enumerate(row))):
                 
