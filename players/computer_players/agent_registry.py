e7ec82c5c00c29b2953b47f202b764bfaba004a3
Neal <nohesselton@gmail.com>
2026-03-30 10:41:36 +0200

    Feat: Added planned game policies to registries

--- players/computer_players/agent_registry.py
+++ players/computer_players/agent_registry.py
@@ -5,7 +5,7 @@ Maps player IDs to agent instances for online or offline training.
 
 RandomAgent allows for RandomPolicy to still be used despite not having an agent
 '''
-from players.computer_players.agents.non_agent import NonAgent # Non agent policy thus pass policy directly
+from players.computer_players.agents.non_agent import NonAgent
 from players.computer_players.agents.rl_dumb_agent import RLDumbAgent
 from players.computer_players.agents.q_learning_agent import QLearningAgent
 
--- players/computer_players/model_policy_registry.py
+++ players/computer_players/model_policy_registry.py
@@ -6,6 +6,8 @@ from players.computer_players.policies.sequence_policy import SequencePolicy
 from players.computer_players.policies.rl_dumb_policy import RLDumbPolicy
 from players.computer_players.policies.q_learning_policy import QLearningPolicy
 from players.computer_players.policies.reverse_sequence_policy import ReverseSequencePolicy
+from players.computer_players.policies.planned_game_policy import PlannedGame
+from players.computer_players.policies.planned_game_policy2 import PlannedGameTwo
 
 
 model_policy_registry = {
@@ -13,4 +15,7 @@ model_policy_registry = {
     "rl_dumb_policy": RLDumbPolicy,
     "q_learning_policy": QLearningPolicy,
     "reverse_sequence_policy": ReverseSequencePolicy,
+    "planned_game_policy": PlannedGame,
+    "planned_game_policy2": PlannedGameTwo,
+
 }
