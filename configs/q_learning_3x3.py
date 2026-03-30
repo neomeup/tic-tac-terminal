dc3ddfd1287b05830c6ec349571eb4e2134f4eed
Neal <nohesselton@gmail.com>
2026-03-30 10:46:02 +0200

    Feat: Skeleton planned game policies implemented

--- configs/q_learning_3x3.py
+++ configs/q_learning_3x3.py
@@ -89,7 +89,9 @@ def get_config() -> GameConfig:
 
         ## If computer player
         ### Policy / Agent
-        policy_type = ["sequence_policy", "reverse_sequence_policy"], #registerized in players/computer_players/model_policy_registry
+        #policy_type = ["sequence_policy", "reverse_sequence_policy"], #registerized in players/computer_players/model_policy_registry
+
+        policy_type = ["planned_game_policy", "planned_game_policy2"], #registerized in players/computer_players/model_policy_registry
         agent_type = ["non_agent", "non_agent"], # registerized in players/computer_players/agent_registry
         reward_type = ["qlearn_tic_tac", "qlearn_tic_tac"], # registerized in simulation/rewards/reward_registry
 
---
+++ players/computer_players/policies/planned_game_policy.py
@@ -0,0 +1,55 @@
+'''
+Base class for AI policies.
+
+Defines choose_move(board_state, player_id).
+'''
+
+from core.move import Move
+from players.computer_players.policies.base_policy import BasePolicy
+
+
+class PlannedGame(BasePolicy):
+
+    def is_x_move(self, board_state, x_move):
+        x = x_move - 1
+
+        filled = sum(
+            1 for row in board_state for col in row if col is not None
+        )
+        if filled == x:
+            return True
+        return False
+
+    def select_action(self, current_player_index: int, board_lst: list[list], config, rng, encoded_state=None):
+        
+        player_id = current_player_index
+
+
+        first_move = self.is_x_move(board_lst, x_move = 1)
+        second_move = self.is_x_move(board_lst, x_move = 2)
+        third_move = self.is_x_move(board_lst, x_move = 3)
+        fourth_move = self.is_x_move(board_lst, x_move = 4)
+        fifth_move = self.is_x_move(board_lst, x_move = 5)
+        sixth_move = self.is_x_move(board_lst, x_move = 6)
+        seventh_move = self.is_x_move(board_lst, x_move = 7)
+        eigth_move = self.is_x_move(board_lst, x_move = 8)
+        ninth_move = self.is_x_move(board_lst, x_move = 9)
+
+
+        if first_move:
+            r = 2
+            c = 0
+            if board_lst[r][c] is None:
+                return Move (player_id=player_id, target_row=r, target_col=c)
+
+        for row_index, row in reversed(list(enumerate(board_lst))):
+            for col_index, cell in reversed(list(enumerate(row))):
+                
+                if cell is None:
+                    r=row_index
+                    c=col_index
+                               
+                    return Move (player_id=player_id, target_row=r, target_col=c)
+
+
+        return Move (player_id=player_id, target_row=r, target_col=c)
